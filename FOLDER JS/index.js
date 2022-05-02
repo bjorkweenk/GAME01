3e9c7f92f75dda0ff032e63372f90ba06647e4bd
Bjork Weenk <bjorkweenk.bw@gmail.com>
2022-05-02 17:27:35 +0200

    change html

--- FOLDER JS/index.js
+++ FOLDER JS/index.js
@@ -2,15 +2,3 @@ console.log("Hi is this working");
 
 const game = new Game();
 window.onload = () => game.init();
-
-
-//const startButton = document.getElementById('start-button');
-
-/*startButton.addEventListener(
-    'click', () => {
-        startButton.textContent = 'RESTART';
-        startButton.blur()
-        game.start();
-    }
-);
-*/
